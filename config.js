86abeacf457945dda50ded10364433f86e3318a1
mvroo1 <jackalkber@gmail.com>
2024-06-22 17:40:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
-global.owner = [['201208386317', '🌩️ ẉa.me/AFROTO‖.peị!‽ 👑', true], 
- ['201208386317', '🌩️ ẉa.me/AFROTO‖.peị!‽ 👑', true],  ['201208386317'], ['201208386317'], ['201208386317'], ['201208386317'], ['201208386317'], ['201208386317'], ['201208386317'], ['201208386317'], ['201208386317']]
+global.owner = [['201208386317', '🌩️ ẉa.me/MVRO‖.peị!‽ 👑', true], 
+ ['201208386317', '🌩️ ẉa.me/MVRO‖.peị!‽ 👑', true],  ['201208386317'], ['201208386317'], ['201208386317'], ['201208386317'], ['201208386317'], ['201208386317'], ['201208386317'], ['201208386317'], ['201208386317']]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = '' //Ejemplo: +201067999523
+global.botNumberCode = '' //Ejemplo: +201208386317
 global.confirmCode = ''
 
 global.animxscans = ['201208386317']
@@ -16,11 +16,11 @@ global.mods = []
 global.prems = []
 
 global.packname = '『 201208386317 』'
-global.author = '『 غوكـو بــ☠️ـوت 』'
-global.wm = '『غـوكـو بــ🐍ـؤؤت 』'
-global.wm2 = '『 غــوكـو بــ💎ـوت 』'
-global.azami = '『غـوكو بــ💀وت 』'
-global.cb = '『 غــوكــو بــ👾ـوت 』'
+global.author = '『 مـــاࢪو بــ☠️ـوت 』'
+global.wm = '『مـــاࢪو بــ🐍ـؤؤت 』'
+global.wm2 = '『 مـــاࢪو بــ💎ـوت 』'
+global.azami = '『مـــاࢪو بــ💀وت 』'
+global.cb = '『 مـــاࢪو بــ👾ـوت 』'
 
 global.vs = 'V2 • 1.0.5'
 global.library = 'Baileys'
@@ -28,8 +28,8 @@ global.baileys = '@whiskeysockets/baileys'
 global.lenguaje = 'Español'
 global.KMA = '╰━━━〔 *🛡️ 1.7.9* 〕━━━━━⬣'
 global.menudi = ['⛶','❏','⫹⫺']
-global.dev = '© 𝑮𝒐𝒌𝒖_BOT'
-global.devnum = '212601064446'
+global.dev = '© 𝙼𝚅𝚁𝙾 𝙱𝙾𝚃'
+global.devnum = '201208386317'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => { unwatchFile(file)
