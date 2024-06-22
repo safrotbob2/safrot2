93b69d4a5f95660ee19a85087d03274e2b2c1ae4
mvroo1 <jackalkber@gmail.com>
2024-06-22 16:44:01 +0300

    Delete plugins directory

--- plugins/دعم.js
+++
@@ -1,19 +0,0 @@
-let handler = async (m, { conn, command, text }) => {
-let love = `
-*لدعم البوت تستطيع ان تفعل:-*
-*1_تستطيع دخول قروب دعم البوت لدعمه.↯↯*
-**
-
-*2_وتستطيع ان تتابع البوت على الانستا:-*
-*https://www.instagram.com/mrwbryh?igsh=MWxwZ2o4N2NkMHN5YQ==* 
-
-*3_وتستطيع دعم المطور من هنا:-*
-*+967774318278*
-
-*وشكرا لكم يا افضل مستخدمين 👾👑*
-`.trim()
-m.reply(love, null, { mentions: conn.parseMention(love) })}
-handler.help = ['estupidez']
-handler.tags = ['fun']
-handler.command = /^(الدعم|دعم)$/i
-export default handler
