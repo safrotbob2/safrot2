6a7a3a846ce1a505b8641252f10f316e65d37567
mvroo1 <jackalkber@gmail.com>
2024-06-22 16:54:25 +0300

    Add files via upload

---
+++ plugins/transformador-togifaud.js
@@ -0,0 +1,18 @@
+var handler = async (m, {conn, usedPrefix, command}) => {
+
+if (!m.quoted) throw `*⚠️رد ع فيديو لتحويله جيفgif*` 
+const q = m.quoted || m
+const mime = (q.msg || q).mimetype || ''
+if (!/(mp4)/.test(mime)) throw `*⚠️ تشابه ${mime} غير مدعوم*` 
+m.reply('*🚀 تم التحويل بوساطه مارو*')
+const media = await q.download()
+conn.sendMessage(m.chat, {video: media, gifPlayback: true, caption: '*🚀🚀*'}, {quoted: m})
+
+}
+handler.help = ['togifaud']
+handler.tags = ['transformador']
+handler.command = ['جيف']
+
+handler.limit = true
+ 
+export default handler
