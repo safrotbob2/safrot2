8e39605826b94f093f4831b0cf089f1e10b9dd64
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-06-30 21:33:47 +0300

    Update اسأل.js

--- plugins/اسأل.js
+++ plugins/اسأل.js
@@ -1,12 +1,21 @@
-let handler = async (m, { command, text }) => m.reply(`
-*◈╎اسـئـل مـارو╎◈*
-  
-*السؤال:* ${text}
-*الاجابة:* ${['اه','ممكن','في الاغلب اه','ف الاغلب لا','لا','مستحيل'].getRandom()}
-`.trim(), null, m.mentionedJid ? {
-mentions: m.mentionedJid
-} : {})
-handler.help = ['pregunta <texto>?']
-handler.tags = ['kerang']
-handler.command = /^هل$/i
+
+import util from  util 
+import path from  path 
+let user = a =>  @  + a.split( @ )[0]
+function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
+if (!text) throw `*أدخــل الـسـؤال !*`
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let x = `${pickRandom([  ممكن , اهه ممكن , اكيد  ,  لا  , لا طبعا ])}`
+let l = Math.floor(Math.random() * x.length);
+let top = `*هــل ${text}*
+
+*الــأجــابـه :* ${x}`.trim()
+conn.sendFile (m.reply (top, null, { mentions: [a]}))}
+handler.help = handler.command = [ هل ]
+handler.tags = [ fun ]
+handler.group = true
+handler.limit = 0
 export default handler
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]}
