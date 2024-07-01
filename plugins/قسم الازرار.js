5abd54392721e7ff28b3afb73c3da84aa4afd676
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-07-01 13:39:56 +0300

    Update قسم الازرار.js

--- plugins/قسم الازرار.js
+++ plugins/قسم الازرار.js
@@ -33,7 +33,7 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
   						  		    		header: '◡̈⃝˼‏👥˹ ━━|قسم الجروبات│━━˼‏👥˹◡̈⃝_',
   										      title: '𝚂𝙰𝙽𝙵𝙾𝚁-𝙱𝙾𝚃',
   									    	  description: '◡̈⃝˼‏👥˹ ━━|الجروبات│━━˼‏👥˹◡̈⃝_',
-  								    		  id: '.safrot1'
+  								    		  id: '.سفروت1'
   						  		    	}
   						  		    ]
   						  			},
